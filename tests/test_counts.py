6f3b4300b37d759ae903094fed893f81a6d0a8b9
John Vandenberg <jayvdb@gmail.com>
2016-05-05 22:59:38 +0700

    Add 2015 tests

--- tests/test_counts.py
+++ tests/test_counts.py
@@ -44,3 +44,24 @@ def test_2012_journal_for_count(connection):
     query = select([func.count(era2012_journal_for.columns.eraid)])
     result = connection.execute(query).scalar()
     assert result == 32864
+
+
+def test_2015_journal_count(connection):
+    era2015_journals = Table('era2015_journal', metadata, autoload=True, autoload_with=connection)
+    query = select([func.count(era2015_journals.columns.eraid)])
+    result = connection.execute(query).scalar()
+    assert result == 16229
+
+
+def test_2015_journal_issn_count(connection):
+    era2015_journal_issn = Table('era2015_journal_issn', metadata, autoload=True, autoload_with=connection)
+    query = select([func.count(era2015_journal_issn.columns.issn)])
+    result = connection.execute(query).scalar()
+    assert result == 28947
+
+
+def test_2015_journal_for_count(connection):
+    era2015_journal_for = Table('era2015_journal_for', metadata, autoload=True, autoload_with=connection)
+    query = select([func.count(era2015_journal_for.columns.eraid)])
+    result = connection.execute(query).scalar()
+    assert result == 27820
